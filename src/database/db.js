b3c0fe28c341495ea2244cdfb321cea29e22778e
Diego Ortiz <2030282@upv.edu.mx>
2024-08-23 11:47:25 -0600

    Database connection file created

---
+++ src/database/db.js
@@ -0,0 +1,20 @@
+import mysql from 'mysql2'
+
+// Connection to the database
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodejs-api'
+})
+
+// Check the connection
+db.connect((err) => {
+  if (err) {
+    throw err
+  }
+  console.log('Connection to the database is established')
+})
+
+// Export the connection
+export default db
